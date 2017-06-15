ce6cd82bbe119d1b62caae02119af80c8f5b0fbe
Torin Kwok <torin@kwok.im>
2017-06-15 18:03:13 +0800

    Backup. Left a set of unit test logic that not passed

--- Tests/NSRegExNamedCaptureGroupTests/NSRegExNamedCaptureGroupTests.swift
+++ Tests/NSRegExNamedCaptureGroupTests/NSRegExNamedCaptureGroupTests.swift
@@ -14,32 +14,35 @@ extension NSRegularExpression {
 
 class NSRegExNamedCaptureGroupTests: XCTestCase {
 
-  func test_1() {
-    let number = "202-555-0136"
-    let USAPhoneNumberPattern = try! NSRegularExpression(
+  class TestSamples_Group1 {
+    static var phoneNumber = "202-555-0136"
+    static var USAPhoneNumberPattern = try! NSRegularExpression(
         pattern: "\\b(?<Area>\\d\\d\\d)-(?:\\d\\d\\d)-(?<Num>\\d\\d\\d\\d)\\b"
       , options: NSRegularExpression.commonOptions
       )
+    }
 
-    USAPhoneNumberPattern.enumerateMatches( in: number, options: [], range: NSMakeRange( 0, number.utf16.count ) ) {
+  func test_1() {
+    TestSamples_Group1.USAPhoneNumberPattern.enumerateMatches( in: TestSamples_Group1.phoneNumber, options: [], range: NSMakeRange( 0, TestSamples_Group1.phoneNumber.utf16.count ) ) {
       checkingResult, _, stopToken in
 
       XCTAssertNotNil( checkingResult )
-
-      guard let checkingResult = checkingResult else {
-        stopToken.pointee = true
-        return
-        }
+      guard let checkingResult = checkingResult else { stopToken.pointee = true; return }
 
       do {
-        let rangesOfCaptured = try USAPhoneNumberPattern.rangesOfNamedCaptureGroups( in: checkingResult )
+        let rangesOfCaptured = try TestSamples_Group1.USAPhoneNumberPattern.rangesOfNamedCaptureGroups( in: checkingResult )
         XCTAssertEqual( rangesOfCaptured.count, checkingResult.numberOfRanges - 1 )
 
         // Tests whether NSRegExNamedCaptureGroup can extract 
-        // all the NCG expressions
-        for ( ncgSubexpr, _ ) in rangesOfCaptured {
-          let pattern = USAPhoneNumberPattern.pattern
+        // all the NCG expressions properly
+        for ( ncgSubexpr, nsRange ) in rangesOfCaptured {
+          let pattern = TestSamples_Group1.USAPhoneNumberPattern.pattern
+          let targetText = TestSamples_Group1.phoneNumber
           XCTAssertNotEqual( pattern.range( of: ncgSubexpr ), nil )
+
+          // let substringCapturedByNCG = targetText[ targetText.range( from: nsRange )! ]
+          // let substringCapturedByOrdi = targetText[ targetText.range( from: checkingResult.range )! ]
+          // XCTAssertEqual( substringCapturedByNCG, substringCapturedByOrdi )
           }
 
         } catch {
