c89cda2a3e3c8de84e13297a20195de13e57e3b5
Torin Kwok <torin@kwok.im>
2017-06-16 21:00:34 +0800

    Go fuck yourself

--- xcode/NSRegExNamedCaptureGroup/NSRegExNamedCaptureGroup iOS/NSRegExNamedCaptureGroup iOS.h
+++ xcode/NSRegExNamedCaptureGroup/NSRegExNamedCaptureGroup iOS/NSRegExNamedCaptureGroup iOS.h
@@ -1,17 +1,17 @@
 //
-//  NSRegExNamedCaptureGroup_iOS.h
-//  NSRegExNamedCaptureGroup_iOS
+//  NSRegExNamedCaptureGroup iOS.h
+//  NSRegExNamedCaptureGroup iOS
 //
 //  Created by Tong G. on 16/06/2017.
 //
 //
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
-//! Project version number for NSRegExNamedCaptureGroup_iOS.
+//! Project version number for NSRegExNamedCaptureGroup iOS.
 FOUNDATION_EXPORT double NSRegExNamedCaptureGroup_iOSVersionNumber;
 
-//! Project version string for NSRegExNamedCaptureGroup_iOS.
+//! Project version string for NSRegExNamedCaptureGroup iOS.
 FOUNDATION_EXPORT const unsigned char NSRegExNamedCaptureGroup_iOSVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <NSRegExNamedCaptureGroup_iOS/PublicHeader.h>
--- xcode/NSRegExNamedCaptureGroup/NSRegExNamedCaptureGroup macOS/NSRegExNamedCaptureGroup macOS.h
+++ xcode/NSRegExNamedCaptureGroup/NSRegExNamedCaptureGroup macOS/NSRegExNamedCaptureGroup macOS.h
@@ -1,17 +1,17 @@
 //
-//  NSRegExNamedCaptureGroup_macOS.h
-//  NSRegExNamedCaptureGroup_macOS
+//  NSRegExNamedCaptureGroup macOS.h
+//  NSRegExNamedCaptureGroup macOS
 //
 //  Created by Tong G. on 16/06/2017.
 //
 //
 
-#import <Foundation/Foundation.h>
+#import <Cocoa/Cocoa.h>
 
-//! Project version number for NSRegExNamedCaptureGroup_macOS.
+//! Project version number for NSRegExNamedCaptureGroup macOS.
 FOUNDATION_EXPORT double NSRegExNamedCaptureGroup_macOSVersionNumber;
 
-//! Project version string for NSRegExNamedCaptureGroup_macOS.
+//! Project version string for NSRegExNamedCaptureGroup macOS.
 FOUNDATION_EXPORT const unsigned char NSRegExNamedCaptureGroup_macOSVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <NSRegExNamedCaptureGroup_macOS/PublicHeader.h>
