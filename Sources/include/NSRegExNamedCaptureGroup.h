9603a24baddfc0ed197cb2ee7733ca27063f6687
Torin Kwok <torin@kwok.im>
2017-06-16 15:11:03 +0800

    for v0.0.2

---
+++ Sources/include/NSRegExNamedCaptureGroup.h
@@ -0,0 +1,19 @@
+//
+//  NSRegExNamedCaptureGroup.h
+//  NSRegExNamedCaptureGroup
+//
+//  Created by Tong G. on 15/06/2017.
+//  Copyright © 2017 Torin Kwok. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NSRegExNamedCaptureGroup.
+FOUNDATION_EXPORT double NSRegExNamedCaptureGroupVersionNumber;
+
+//! Project version string for NSRegExNamedCaptureGroup.
+FOUNDATION_EXPORT const unsigned char NSRegExNamedCaptureGroupVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NSRegExNamedCaptureGroup/PublicHeader.h>
+
+
