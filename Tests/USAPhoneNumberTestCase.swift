994cac617e83de8d13a9c3091c111567760414f9
Torin Kwok <torin@kwok.im>
2017-06-18 15:11:11 +0800

    Backup during refactor our unit test cases

--- Tests/USAPhoneNumberTestCase.swift
+++ Tests/USAPhoneNumberTestCase.swift
@@ -3,43 +3,26 @@ import XCTest
 
 class USAPhoneNumberTestCase: NSRegExNamedCaptureGroupTests {
 
-  class TestSamples_Group1 {
-    static let phoneNumber = "202-555-0136"
-
-    static let groupNameArea = "Area"
-    static let groupNameExch = "Exch"
-    static let groupNameNum = "Num"
-
-    static let areaPattern = "(?<\(groupNameArea)>\\d\\d\\d)"
-    static let exchPattern = "(?:\\d\\d\\d)"
-    static let numPattern = "(?<\(groupNameNum)>\\d\\d\\d\\d)"
-
-    static let USAPhoneNumberPattern = try! NSRegularExpression(
-        pattern: "\\b\(areaPattern)-\(exchPattern)-\(numPattern)\\b"
-      , options: NSRegularExpression.commonOptions
-      )
-    }
-
   func testArrayBasedAPI_01() {
-    let matches = TestSamples_Group1.USAPhoneNumberPattern.matches(
-        in: TestSamples_Group1.phoneNumber
+    let matches = USAPhoneNumberSamples.USAPhoneNumberPattern.matches(
+        in: USAPhoneNumberSamples.phoneNumber
       , options: []
-      , range: NSMakeRange( 0, TestSamples_Group1.phoneNumber.utf16.count )
+      , range: NSMakeRange( 0, USAPhoneNumberSamples.phoneNumber.utf16.count )
       )
 
     for match in matches {
       XCTAssert( _compareRange( in: match, byGroupName: nil, with: 0 ) )
-      XCTAssert( _compareRange( in: match, byGroupName: TestSamples_Group1.groupNameArea, with: 1 ) )
-      XCTAssert( _isRangeInvalid( in: match, byGroupName: TestSamples_Group1.groupNameExch ) )
-      XCTAssert( _compareRange( in: match, byGroupName: TestSamples_Group1.groupNameNum, with: 2 ) )
+      XCTAssert( _compareRange( in: match, byGroupName: USAPhoneNumberSamples.groupNameArea, with: 1 ) )
+      XCTAssert( _isRangeInvalid( in: match, byGroupName: USAPhoneNumberSamples.groupNameExch ) )
+      XCTAssert( _compareRange( in: match, byGroupName: USAPhoneNumberSamples.groupNameNum, with: 2 ) )
       }
     }
 
   func testBlockEnumerationBasedAPI_01() {
-    TestSamples_Group1.USAPhoneNumberPattern.enumerateMatches(
-        in: TestSamples_Group1.phoneNumber
+    USAPhoneNumberSamples.USAPhoneNumberPattern.enumerateMatches(
+        in: USAPhoneNumberSamples.phoneNumber
       , options: []
-      , range: NSMakeRange( 0, TestSamples_Group1.phoneNumber.utf16.count )
+      , range: NSMakeRange( 0, USAPhoneNumberSamples.phoneNumber.utf16.count )
       ) { checkingResult, _, stopToken in
 
       XCTAssertNotNil( checkingResult )
@@ -49,17 +32,17 @@ class USAPhoneNumberTestCase: NSRegExNamedCaptureGroupTests {
         }
 
       XCTAssert( _compareRange( in: checkingResult, byGroupName: nil, with: 0 ) )
-      XCTAssert( _compareRange( in: checkingResult, byGroupName: TestSamples_Group1.groupNameArea, with: 1 ) )
-      XCTAssert( _isRangeInvalid( in: checkingResult, byGroupName: TestSamples_Group1.groupNameExch ) )
-      XCTAssert( _compareRange( in: checkingResult, byGroupName: TestSamples_Group1.groupNameNum, with: 2 ) )
+      XCTAssert( _compareRange( in: checkingResult, byGroupName: USAPhoneNumberSamples.groupNameArea, with: 1 ) )
+      XCTAssert( _isRangeInvalid( in: checkingResult, byGroupName: USAPhoneNumberSamples.groupNameExch ) )
+      XCTAssert( _compareRange( in: checkingResult, byGroupName: USAPhoneNumberSamples.groupNameNum, with: 2 ) )
       }
     }
 
   func testFirstMatchAPI_01() {
-    let result = TestSamples_Group1.USAPhoneNumberPattern.firstMatch(
-        in: TestSamples_Group1.phoneNumber
+    let result = USAPhoneNumberSamples.USAPhoneNumberPattern.firstMatch(
+        in: USAPhoneNumberSamples.phoneNumber
       , options: []
-      , range: NSMakeRange( 0, TestSamples_Group1.phoneNumber.utf16.count ) )
+      , range: NSMakeRange( 0, USAPhoneNumberSamples.phoneNumber.utf16.count ) )
 
     XCTAssertNotNil( result )
     guard let firstMatch = result else {
@@ -67,9 +50,9 @@ class USAPhoneNumberTestCase: NSRegExNamedCaptureGroupTests {
       }
 
     XCTAssert( _compareRange( in: firstMatch, byGroupName: nil, with: 0 ) )
-    XCTAssert( _compareRange( in: firstMatch, byGroupName: TestSamples_Group1.groupNameArea, with: 1 ) )
-    XCTAssert( _isRangeInvalid( in: firstMatch, byGroupName: TestSamples_Group1.groupNameExch ) )
-    XCTAssert( _compareRange( in: firstMatch, byGroupName: TestSamples_Group1.groupNameNum, with: 2 ) )
+    XCTAssert( _compareRange( in: firstMatch, byGroupName: USAPhoneNumberSamples.groupNameArea, with: 1 ) )
+    XCTAssert( _isRangeInvalid( in: firstMatch, byGroupName: USAPhoneNumberSamples.groupNameExch ) )
+    XCTAssert( _compareRange( in: firstMatch, byGroupName: USAPhoneNumberSamples.groupNameNum, with: 2 ) )
     }
 
   static var allTests = [
