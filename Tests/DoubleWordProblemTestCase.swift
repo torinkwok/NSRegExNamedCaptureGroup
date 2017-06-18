0ddd7acb0436d8229c0c5651976c306708ec4369
Torin Kwok <torin@kwok.im>
2017-06-18 15:27:21 +0800

    Introduced DoubleWordProblemTestCase

---
+++ Tests/DoubleWordProblemTestCase.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import NSRegExNamedCaptureGroup
+
+fileprivate class DoubleWordProblemTestCase: NSRegExNamedCaptureGroupTests {
+  // static var allTests = [
+  //     ( "testArrayBasedAPI_01", testArrayBasedAPI_01 )
+  //   ]
+  }
+
+fileprivate extension DoubleWordProblemTestCase {
+  
+  }
